3319f289ac850db82475c33884140b6b2603e831
tmknom <tmk.nom@gmail.com>
2020-05-03 07:50:16 +0900

    Upgraded examples/minimal to support Terraform 0.12

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,27 +1,27 @@
 module "ecs_fargate" {
   source                = "../../"
   name                  = "example"
-  container_name        = "${local.container_name}"
-  container_port        = "${local.container_port}"
-  cluster               = "${aws_ecs_cluster.example.arn}"
-  subnets               = ["${module.vpc.public_subnet_ids}"]
-  target_group_arn      = "${module.alb.alb_target_group_arn}"
-  vpc_id                = "${module.vpc.vpc_id}"
-  container_definitions = "${data.template_file.default.rendered}"
+  container_name        = local.container_name
+  container_port        = local.container_port
+  cluster               = aws_ecs_cluster.example.arn
+  subnets               = [module.vpc.public_subnet_ids]
+  target_group_arn      = module.alb.alb_target_group_arn
+  vpc_id                = module.vpc.vpc_id
+  container_definitions = data.template_file.default.rendered
 }
 
 data "template_file" "default" {
-  template = "${file("${path.module}/container_definitions.json")}"
+  template = file("${path.module}/container_definitions.json")
 
-  vars {
-    container_name = "${local.container_name}"
-    container_port = "${local.container_port}"
+  vars = {
+    container_name = local.container_name
+    container_port = local.container_port
   }
 }
 
 locals {
   container_name = "example"
-  container_port = "${module.alb.alb_target_group_port}"
+  container_port = module.alb.alb_target_group_port
 }
 
 resource "aws_ecs_cluster" "example" {
@@ -31,9 +31,9 @@ resource "aws_ecs_cluster" "example" {
 module "alb" {
   source                     = "git::https://github.com/tmknom/terraform-aws-alb.git?ref=tags/1.4.1"
   name                       = "ecs-fargate"
-  vpc_id                     = "${module.vpc.vpc_id}"
-  subnets                    = ["${module.vpc.public_subnet_ids}"]
-  access_logs_bucket         = "${module.s3_lb_log.s3_bucket_id}"
+  vpc_id                     = module.vpc.vpc_id
+  subnets                    = [module.vpc.public_subnet_ids]
+  access_logs_bucket         = module.s3_lb_log.s3_bucket_id
   enable_https_listener      = false
   enable_http_listener       = true
   enable_deletion_protection = false
@@ -42,7 +42,7 @@ module "alb" {
 module "s3_lb_log" {
   source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/1.0.0"
   name                  = "s3-lb-log-ecs-fargate-${data.aws_caller_identity.current.account_id}"
-  logging_target_bucket = "${module.s3_access_log.s3_bucket_id}"
+  logging_target_bucket = module.s3_access_log.s3_bucket_id
   force_destroy         = true
 }
 
@@ -54,10 +54,10 @@ module "s3_access_log" {
 
 module "vpc" {
   source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
-  cidr_block                = "${local.cidr_block}"
+  cidr_block                = local.cidr_block
   name                      = "ecs-fargate"
-  public_subnet_cidr_blocks = ["${cidrsubnet(local.cidr_block, 8, 0)}", "${cidrsubnet(local.cidr_block, 8, 1)}"]
-  public_availability_zones = ["${data.aws_availability_zones.available.names}"]
+  public_subnet_cidr_blocks = [cidrsubnet(local.cidr_block, 8, 0), cidrsubnet(local.cidr_block, 8, 1)]
+  public_availability_zones = [data.aws_availability_zones.available.names]
 }
 
 locals {
