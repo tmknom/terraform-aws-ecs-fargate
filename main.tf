f8fd14c45968385485def481d0ff7b70322c0220
tmknom <tmk.nom@gmail.com>
2020-05-03 07:45:08 +0900

    Upgraded module to support Terraform 0.12

--- main.tf
+++ main.tf
@@ -13,48 +13,48 @@
 
 # https://www.terraform.io/docs/providers/aws/r/ecs_service.html
 resource "aws_ecs_service" "default" {
-  count = "${var.enabled}"
+  count = var.enabled
 
   # The name of your service. Up to 255 letters (uppercase and lowercase), numbers, hyphens, and underscores are allowed.
   # Service names must be unique within a cluster, but you can have similarly named services
   # in multiple clusters within a region or across multiple regions.
-  name = "${var.name}"
+  name = var.name
 
   # The family and revision (family:revision) or full ARN of the task definition to run in your service.
   # If a revision is not specified, the latest ACTIVE revision is used.
-  task_definition = "${aws_ecs_task_definition.default.arn}"
+  task_definition = aws_ecs_task_definition.default[0].arn
 
   # The short name or full Amazon Resource Name (ARN) of the cluster on which to run your service.
   # If you do not specify a cluster, the default cluster is assumed.
-  cluster = "${var.cluster}"
+  cluster = var.cluster
 
   # The number of instantiations of the specified task definition to place and keep running on your cluster.
-  desired_count = "${var.desired_count}"
+  desired_count = var.desired_count
 
   # The maximumPercent parameter represents an upper limit on the number of your service's tasks
   # that are allowed in the RUNNING or PENDING state during a deployment,
   # as a percentage of the desiredCount (rounded down to the nearest integer).
-  deployment_maximum_percent = "${var.deployment_maximum_percent}"
+  deployment_maximum_percent = var.deployment_maximum_percent
 
   # The minimumHealthyPercent represents a lower limit on the number of your service's tasks
   # that must remain in the RUNNING state during a deployment,
   # as a percentage of the desiredCount (rounded up to the nearest integer).
-  deployment_minimum_healthy_percent = "${var.deployment_minimum_healthy_percent}"
+  deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
 
   deployment_controller {
     # The deployment controller type to use. Valid values: CODE_DEPLOY, ECS.
-    type = "${var.deployment_controller_type}"
+    type = var.deployment_controller_type
   }
 
   # The network configuration for the service. This parameter is required for task definitions
   # that use the awsvpc network mode to receive their own Elastic Network Interface.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-networking.html
   network_configuration {
-    subnets         = ["${var.subnets}"]
-    security_groups = ["${aws_security_group.default.id}"]
+    subnets         = var.subnets
+    security_groups = [aws_security_group.default[0].id]
 
     # Whether the task's elastic network interface receives a public IP address.
-    assign_public_ip = "${var.assign_public_ip}"
+    assign_public_ip = var.assign_public_ip
   }
 
   # Note: As a result of an AWS limitation, a single load_balancer can be attached to the ECS service at most.
@@ -67,26 +67,26 @@ resource "aws_ecs_service" "default" {
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-load-balancing.html#load-balancing-concepts
   load_balancer {
     # The ARN of the Load Balancer target group to associate with the service.
-    target_group_arn = "${var.target_group_arn}"
+    target_group_arn = var.target_group_arn
 
     # The name of the container to associate with the load balancer (as it appears in a container definition).
-    container_name = "${var.container_name}"
+    container_name = var.container_name
 
     # The port on the container to associate with the load balancer.
-    container_port = "${var.container_port}"
+    container_port = var.container_port
   }
 
   # If your service's tasks take a while to start and respond to Elastic Load Balancing health checks,
   # you can specify a health check grace period of up to 7,200 seconds. This grace period can prevent
   # the service scheduler from marking tasks as unhealthy and stopping them before they have time to come up.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/service-create-loadbalancer-rolling.html
-  health_check_grace_period_seconds = "${var.health_check_grace_period_seconds}"
+  health_check_grace_period_seconds = var.health_check_grace_period_seconds
 
   # You can use either the version number (for example, 1.4.0) or LATEST.
   # If you specify LATEST, your tasks use the most current platform version available,
   # which may not be the most recent platform version.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/platform_versions.html
-  platform_version = "${var.platform_version}"
+  platform_version = var.platform_version
 
   # The launch type on which to run your service.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_types.html
@@ -102,7 +102,7 @@ resource "aws_ecs_service" "default" {
   lifecycle {
     # Ignore any changes to that count caused externally (e.g. Application Autoscaling).
     # https://www.terraform.io/docs/providers/aws/r/ecs_service.html#ignoring-changes-to-desired-count
-    ignore_changes = ["desired_count"]
+    ignore_changes = [desired_count]
   }
 }
 
@@ -114,11 +114,11 @@ resource "aws_ecs_service" "default" {
 #
 # https://www.terraform.io/docs/providers/aws/r/security_group.html
 resource "aws_security_group" "default" {
-  count = "${var.enabled}"
+  count = var.enabled
 
-  name   = "${local.security_group_name}"
-  vpc_id = "${var.vpc_id}"
-  tags   = "${merge(map("Name", local.security_group_name), var.tags)}"
+  name   = local.security_group_name
+  vpc_id = var.vpc_id
+  tags   = merge({ "Name" = local.security_group_name }, var.tags)
 }
 
 locals {
@@ -127,25 +127,25 @@ locals {
 
 # https://www.terraform.io/docs/providers/aws/r/security_group_rule.html
 resource "aws_security_group_rule" "ingress" {
-  count = "${var.enabled}"
+  count = var.enabled
 
   type              = "ingress"
-  from_port         = "${var.container_port}"
-  to_port           = "${var.container_port}"
+  from_port         = var.container_port
+  to_port           = var.container_port
   protocol          = "tcp"
-  cidr_blocks       = ["${var.ingress_cidr_blocks}"]
-  security_group_id = "${aws_security_group.default.id}"
+  cidr_blocks       = var.ingress_cidr_blocks
+  security_group_id = aws_security_group.default[0].id
 }
 
 resource "aws_security_group_rule" "egress" {
-  count = "${var.enabled}"
+  count = var.enabled
 
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.default.id}"
+  security_group_id = aws_security_group.default[0].id
 }
 
 # ECS Task Definitions
@@ -154,40 +154,40 @@ resource "aws_security_group_rule" "egress" {
 
 # https://www.terraform.io/docs/providers/aws/r/ecs_task_definition.html
 resource "aws_ecs_task_definition" "default" {
-  count = "${var.enabled}"
+  count = var.enabled
 
   # A unique name for your task definition.
-  family = "${var.name}"
+  family = var.name
 
   # The ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume.
-  execution_role_arn = "${var.create_ecs_task_execution_role ? join("", aws_iam_role.default.*.arn) : var.ecs_task_execution_role_arn}"
+  execution_role_arn = var.create_ecs_task_execution_role ? join("", aws_iam_role.default.*.arn) : var.ecs_task_execution_role_arn
 
   # A list of container definitions in JSON format that describe the different containers that make up your task.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#container_definitions
-  container_definitions = "${var.container_definitions}"
+  container_definitions = var.container_definitions
 
   # The number of CPU units used by the task.
   # It can be expressed as an integer using CPU units, for example 1024, or as a string using vCPUs, for example 1 vCPU or 1 vcpu.
   # String values are converted to an integer indicating the CPU units when the task definition is registered.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size
-  cpu = "${var.cpu}"
+  cpu = var.cpu
 
   # The amount of memory (in MiB) used by the task.
   # It can be expressed as an integer using MiB, for example 1024, or as a string using GB, for example 1GB or 1 GB.
   # String values are converted to an integer indicating the MiB when the task definition is registered.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#task_size
-  memory = "${var.memory}"
+  memory = var.memory
 
   # The launch type that the task is using.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#requires_compatibilities
-  requires_compatibilities = ["${var.requires_compatibilities}"]
+  requires_compatibilities = var.requires_compatibilities
 
   # Fargate infrastructure support the awsvpc network mode.
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_definition_parameters.html#network_mode
   network_mode = "awsvpc"
 
   # A mapping of tags to assign to the resource.
-  tags = "${merge(map("Name", var.name), var.tags)}"
+  tags = merge({ "Name" = var.name }, var.tags)
 }
 
 # ECS Task Execution IAM Role
@@ -196,13 +196,13 @@ resource "aws_ecs_task_definition" "default" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_role.html
 resource "aws_iam_role" "default" {
-  count = "${local.enabled_ecs_task_execution}"
+  count = local.enabled_ecs_task_execution
 
-  name               = "${local.iam_name}"
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-  path               = "${var.iam_path}"
-  description        = "${var.description}"
-  tags               = "${merge(map("Name", local.iam_name), var.tags)}"
+  name               = local.iam_name
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  path               = var.iam_path
+  description        = var.description
+  tags               = merge({ "Name" = local.iam_name }, var.tags)
 }
 
 data "aws_iam_policy_document" "assume_role_policy" {
@@ -218,25 +218,25 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 # https://www.terraform.io/docs/providers/aws/r/iam_policy.html
 resource "aws_iam_policy" "default" {
-  count = "${local.enabled_ecs_task_execution}"
+  count = local.enabled_ecs_task_execution
 
-  name        = "${local.iam_name}"
-  policy      = "${data.aws_iam_policy.ecs_task_execution.policy}"
-  path        = "${var.iam_path}"
-  description = "${var.description}"
+  name        = local.iam_name
+  policy      = data.aws_iam_policy.ecs_task_execution.policy
+  path        = var.iam_path
+  description = var.description
 }
 
 # https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
 resource "aws_iam_role_policy_attachment" "default" {
-  count = "${local.enabled_ecs_task_execution}"
+  count = local.enabled_ecs_task_execution
 
-  role       = "${aws_iam_role.default.name}"
-  policy_arn = "${aws_iam_policy.default.arn}"
+  role       = aws_iam_role.default[0].name
+  policy_arn = aws_iam_policy.default[0].arn
 }
 
 locals {
   iam_name                   = "${var.name}-ecs-task-execution"
-  enabled_ecs_task_execution = "${var.enabled && var.create_ecs_task_execution_role ? 1 : 0}"
+  enabled_ecs_task_execution = var.enabled && var.create_ecs_task_execution_role ? 1 : 0
 }
 
 data "aws_iam_policy" "ecs_task_execution" {
